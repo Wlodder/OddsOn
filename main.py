cc7f617cb4e11dddb2b0d0e021804dee9ba58a6d
Wolodymyr Krywonos <wolodyak@hotmail.co.uk>
2020-06-26 00:50:36 +0100

    add code

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import flask as Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/hello')
+def hello():
+    return 'Hello'
